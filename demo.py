6166708f9d767f3e3d68e282923efe0130e97a38
HuyNguyen <nguyendoanhoanghuy1412@gmail.com>
2022-09-15 10:22:16 +0700

    function demo

--- demo.py
+++ demo.py
@@ -1,2 +1,57 @@
-print("hello")
-print("Change")
+# Function 
+# print("Hello world")
+
+# Arguments
+# print("Something")
+
+# Bug Syntax
+# input("What is your name"
+
+# Bug Logic
+# a = input("a ")
+# b= input("b ")
+# print(a + b)
+
+#Return value - Variable
+
+# name = input("What is your name")
+# print("Hello, "+name)
+
+#Comment
+"""
+something will print out the result
+"""
+
+# name = input("What is your name?")
+# print("STRIP ", name.strip())
+
+# name = input("What is your name?")
+# print("CAPITALIZE ", name.capitalize())
+
+# name = input("What is your name?")
+# print("TITLE ", name.title())
+
+# name = input("What is your name?")
+# print("SPLIT ", name.split())
+
+# a = 3
+# b=7
+# print("a + b = ", a+b)
+# print("a - b = ", a-b)
+# print("a * b = ", a*b)
+# print("a / b = ", a/b)
+# print("a // b = ", a//b)
+# print("a % b = ", a%b)
+# print("a ** b = ", a**b)
+
+
+# def PrintHello(name = "admin"):
+# 	print("Hello, "+ name)
+
+# def Sum(a, b):
+# 	return int(a) + int(b)
+
+# def GetName(fullName=""):
+# 	firstName = fullName.split()[0]
+# 	lastName = fullName.split()[1]
+# 	return firstName, lastName
