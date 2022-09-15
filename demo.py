ae7a2a210f5a4c6c3948576f3c64c6fa5c18edb5
HuyNguyen <nguyendoanhoanghuy1412@gmail.com>
2022-09-15 10:40:48 +0700

    condition

--- demo.py
+++ demo.py
@@ -55,3 +55,78 @@ something will print out the result
 # 	firstName = fullName.split()[0]
 # 	lastName = fullName.split()[1]
 # 	return firstName, lastName
+
+# IF
+
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+
+# if x < y:
+#   print("x is less than y")
+# if x > y:
+#   print("x is greater than y")
+# if x == y:
+#   print("x is equal to y")
+
+# ELSE IF 
+
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+
+# if x < y:
+#   print("x is less than y")
+# elif x > y:
+#   print("x is greater than y")
+# elif x == y:
+#   print("x is equal to y")
+
+# ELSE 
+
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+
+# if x < y:
+#   print("x is less than y")
+# elif x > y:
+#   print("x is greater than y")
+# else:
+#   print("x is equal to y")
+
+# OR 
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+
+# if x < y or x > y:
+#   print("x is not equal to y")
+# else:
+#   print("x equal to y")
+
+# AND 
+# score = int(input("Score: "))
+
+# if score >= 90 and score <=100:
+#   print("Grade A")
+# elif score >= 80 and score < 90:
+#   print("Grade B")
+# elif score >= 70 and score < 80:
+#   print("Grade C")
+# elif score >= 60 and score < 70:
+#   print("Grade D")
+# else:
+#   print("Grade F")
+
+# MATCH 
+
+# name = input("What's your name? ")
+
+# match name:
+#   case "Harry":
+#     print("Gryffindor")
+#   case "Hermione":
+#     print("Gryffindor")
+#   case "Ron":
+#     print("Gryffindor")
+#   case "Draco":
+#     print("Slytherin")
+#   case _:
+#     print("Who?")
